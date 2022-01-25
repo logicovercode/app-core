45fcbaf78827f1156d8953f96f603b49fda5508d
techLeadAtLogicOverCode <techLead@logicovercode.com>
2022-01-25 13:17:04 +0530

    OS_SLASH added

--- core/com/logicovercode/core/CheckOs.scala
+++ core/com/logicovercode/core/CheckOs.scala
@@ -4,8 +4,12 @@ trait CheckOs {
 
   val OS_PROPERTY = "os.name"
 
+  val OS_SLASH = if(isWindowsMachine( sys.props.get(OS_PROPERTY) )) "\\" else "/"
+
   def isWindowsMachine(os: Option[String]): Boolean = (for {
     osName <- os
     os_name = osName.toLowerCase
   } yield os_name.contains("win")).getOrElse(false)
 }
+
+
--- core/com/logicovercode/core/CodePaths.scala
+++ core/com/logicovercode/core/CodePaths.scala
@@ -3,7 +3,10 @@ package com.logicovercode.core
 import better.files.Dsl.cwd
 import better.files.File
 
-object CodePaths {
+object CodePaths extends CheckOs {
+
+
+
   case class StackInfo(index : Int, className : String, fileName : String){
     val packagePrefix = {
       val arr = className.split("\\.")
@@ -41,15 +44,15 @@ object CodePaths {
   }
 
   def classPackagePath(klass : Class[_]) : String = {
-    val kpPath = klass.getPackage.getName.replace(".", "/")
-    "/" + kpPath
+    val kpPath = klass.getPackage.getName.replace(".", OS_SLASH)
+    OS_SLASH + kpPath
   }
 
   def packagePathInContext(): String = {
     val effectiveStack = effectiveStackTraceElement()
     val className = effectiveStack.getClassName
     val i = className.lastIndexOf(".")
-    className.substring(0, i).replace(".", "/")
+    className.substring(0, i).replace(".", OS_SLASH)
   }
 
   def sourceFile(): File = {
